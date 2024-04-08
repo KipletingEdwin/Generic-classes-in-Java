547bef890b738c9bb8276cd425f7dfa3bc404e32
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-08 16:06:11 +0300

    finalize on generics

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //bounded types  =  you can create the objects of a generic class to have data
+        //                  of specific derived types ex. Number
+
+        MyGenericClass <Integer, Integer> myInt = new MyGenericClass<>(1,2);
+        MyGenericClass <Double, Double> myDouble = new MyGenericClass<>(3.14, 3.87);
+        MyGenericClass <Character, Character> myChar = new MyGenericClass<>('@', '#');
+        MyGenericClass <String, Character> myString = new MyGenericClass<>("Hello", 'H');
+
+//        ArrayList<String> myFriends = new ArrayList<>();
+
+        System.out.println(myInt.getValue());
+        System.out.println(myDouble.getValue());
+        System.out.println(myChar.getValue());
+        System.out.println(myString.getValue());
+
+
+
     }
 }
